79275371c352192557eb911f7060c842b2991f72
SukruYuksel <yksl3743dc@gmail.com>
2024-01-04 22:56:31 +0100

    Sum Methode wurde hingefügen

--- src/SumArrayElement.java
+++ src/SumArrayElement.java
@@ -1,6 +1,22 @@
 public class SumArrayElement {
     public static void main(String[] args) {
-        System.out.println("Array Toplamini bulabilen Programa hosgeldiniz");
+        System.out.println("Array Toplamini ve Reversesini bulan  Programa hosgeldiniz");
         System.out.println("Simdi basliyoruz");
+        System.out.println("Simdi basliyoruz");
+
+        int[] sayilar = {2, 4, 6, 8, 10};
+        sumAndReverseArray(sayilar);
+
     }
+
+    public static void sumAndReverseArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("sum = " + sum);
+
+
+    }
+
 }
