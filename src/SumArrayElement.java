a3c89fffdb182775461ebe40c2bd7d7e56893632
SukruYuksel <yksl3743dc@gmail.com>
2024-01-04 22:29:49 +0100

    SumArrayElement.java eklendi

---
+++ src/SumArrayElement.java
@@ -0,0 +1,6 @@
+public class SumArrayElement {
+    public static void main(String[] args) {
+        System.out.println("Array Toplamini bulabilen Programa hosgeldiniz");
+        System.out.println("Simdi basliyoruz");
+    }
+}
